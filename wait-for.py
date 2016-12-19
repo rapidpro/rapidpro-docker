236fe4e7d5db8418ec7a1fce8ceac53119616a5e
Simon de Haan <simon@praekelt.org>
2016-12-19 13:45:50 +0100

    docker-compose is not working for this

--- wait-for.py
+++ wait-for.py
@@ -1,37 +1,26 @@
 import click
 import re
 import time
-import subprocess
+import docker
+from docker.errors import NotFound
 
 
-def call(cmd, timeout=60):
-    return subprocess.run(
-        cmd,
-        timeout=timeout,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        universal_newlines=True)
-
-
-def wait_for(cmd, pattern, timeout=60, sleep=1, verbose=False):
-    process = call(cmd, timeout=timeout)
+def wait_for(output, pattern, timeout=60, sleep=1, verbose=False):
     time_spent = 0
-    while not re.findall(pattern, process.stdout):
+    while not re.findall(pattern, output.decode('utf-8')):
         time.sleep(sleep)
         time_spent += sleep
         if time_spent > timeout:
             if verbose:
-                print(process.stdout)
+                print(output)
             raise click.ClickException(
                 'Timeout of %s reached for %s' % (timeout, ' '.join(cmd)))
     return True
 
 
 @click.command()
-@click.option("--service", type=click.STRING,
-              help="Which service to wait for")
-@click.option("--find", type=click.STRING,
-              help="What regex to find in the services logs (does re.findall)")
+@click.option("--name", type=click.STRING,
+              help="Which container to wait for")
 @click.option("--timeout", type=click.INT,
               help="How long to wait for.",
               default=60)
@@ -41,13 +30,18 @@ def wait_for(cmd, pattern, timeout=60, sleep=1, verbose=False):
 @click.option("--verbose/--no-verbose",
               help="Log the stdout captured on timeout.",
               default=False)
-def cmd(service, find, timeout, sleep, verbose):
-    if wait_for(["docker-compose", "ps", service], r"Up",
-                timeout=timeout, verbose=verbose):
-        if find:
-            return wait_for(
-                ["docker-compose", "logs", "--no-color", service],
-                find, timeout=timeout, verbose=verbose)
+@click.argument("find", type=click.STRING)
+def cmd(name, timeout, sleep, verbose, find):
+    client = docker.from_env()
+    try:
+        container = client.containers.get(name)
+    except NotFound:
+        raise click.ClickException('Container %s does not exist.' % (
+            name,))
+
+    if find:
+        logs = container.logs()
+        return wait_for(logs, find, timeout=timeout, verbose=verbose)
 
 
 if __name__ == '__main__':
