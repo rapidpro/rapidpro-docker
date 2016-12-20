27359669b4fe1d5c93dd3bda3b39fd4d1cd133f1
Simon de Haan <simon@praekelt.org>
2016-12-20 16:35:19 +0100

    show log output while waiting

--- wait-for.py
+++ wait-for.py
@@ -11,6 +11,7 @@ that take some time to boot/provision.
         --timeout=60 \
         --sleep=1 \
         --verbose \
+        --monitor \
         "this thing I'm looking for" \
         "this other things I'm looking for"
 
@@ -18,6 +19,8 @@ that take some time to boot/provision.
 Will fail if "this thing I'm looking for" isn't spotted in the container logs
 within 60 seconds of starting, it'll check every 1 second. If it fails
 it will dump the captured log output to stdout for inspection.
+
+Adding `--monitor` will print the last 2 lines of logs instead of a full stop.
 
 Timeouts can be disabled by setting it to zero or lower.
 
@@ -29,15 +32,19 @@ import docker
 from docker.errors import NotFound
 
 
-def wait_for(container, pattern, timeout=60, sleep=1, verbose=False):
+def wait_for(container, pattern, timeout=60, sleep=1,
+             verbose=False, monitor=False):
     time_spent = 0
     click.secho("\n%s: " % (pattern,), fg='blue', nl=False)
     while True:
-        logs = container.logs()
-        if re.findall(pattern, logs.decode('utf-8')):
+        logs = container.logs().rstrip().decode('utf-8')
+        if re.findall(pattern, logs):
             break
         time.sleep(sleep)
-        click.secho(".", fg="green", nl=False)
+        if monitor:
+            click.secho('\n'.join(logs.split('\n')[-2:]), fg='yellow')
+        else:
+            click.secho(".", fg="green", nl=False)
         time_spent += sleep
         if timeout > 0 and time_spent > timeout:
             click.secho(".", fg="red")
@@ -60,8 +67,11 @@ def wait_for(container, pattern, timeout=60, sleep=1, verbose=False):
 @click.option("--verbose/--no-verbose",
               help="Log the stdout captured on timeout.",
               default=False)
+@click.option('--monitor/--no-monitor',
+              help='Print the last line of log output every sleep interval',
+              default=False)
 @click.argument("patterns", type=click.STRING, nargs=-1)
-def cmd(name, timeout, sleep, verbose, patterns):
+def cmd(name, timeout, sleep, verbose, monitor, patterns):
     client = docker.from_env()
     try:
         container = client.containers.get(name)
@@ -71,7 +81,8 @@ def cmd(name, timeout, sleep, verbose, patterns):
 
     for pattern in patterns:
         wait_for(container, pattern,
-                 timeout=timeout, sleep=sleep, verbose=verbose)
+                 timeout=timeout, sleep=sleep,
+                 verbose=verbose, monitor=monitor)
 
 
 if __name__ == '__main__':
