b31b3ac68f9fec0bd8b6a20ecbfa6953be8e08e3
Simon de Haan <simon@praekelt.org>
2016-12-21 15:39:00 +0100

    deploy script uhappy

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,8 @@
 
 set -ex
 
-docker tag "$image:$version"
-docker push "$image:$version"
+echo "Tagging $1:$2"
+docker tag "$1" "$1:$2"
+echo "Pushing $1:$2 to Docker Hub"
+docker push "$1:$2"
+echo "Done"
