e2d72599af7fa9d698e0db6b796eee34e7887352
Simon de Haan <simon@praekelt.org>
2017-03-24 13:56:49 +0100

    reinstance venv

--- stack/startup.sh
+++ stack/startup.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 set -ex # fail on any error & print commands as they're run
 if [ "x$MANAGEPY_COLLECTSTATIC" = "xon" ]; then
-	python manage.py collectstatic --noinput --no-post-process
+	/venv/bin/python manage.py collectstatic --noinput --no-post-process
 fi
 if [ "x$MANAGEPY_COMPRESS" = "xon" ]; then
-	python manage.py compress --extension=".haml" --force -v0
+	/venv/bin/python manage.py compress --extension=".haml" --force -v0
 fi
 if [ "x$MANAGEPY_INIT_DB" = "xon" ]; then
 	set +x  # make sure the password isn't echoed to stdout
 	echo "*:*:*:*:$(echo \"$DATABASE_URL\" | cut -d'@' -f1 | cut -d':' -f3)" > $HOME/.pgpass
 	set -x
 	chmod 0600 $HOME/.pgpass
-	python manage.py dbshell < init_db.sql
+	/venv/bin/python manage.py dbshell < init_db.sql
 	rm $HOME/.pgpass
 fi
 if [ "x$MANAGEPY_MIGRATE" = "xon" ]; then
-	python manage.py migrate
+	/venv/bin/python manage.py migrate
 fi
 $STARTUP_CMD
