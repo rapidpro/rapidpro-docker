f8c5974c4a2ce9fc64d27b3aab7c466dd5d1b740
Simon de Haan <simon@praekelt.org>
2017-03-24 14:48:08 +0100

    build statics at build time

--- stack/startup.sh
+++ stack/startup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -ex # fail on any error & print commands as they're run
-if [ "x$MANAGEPY_COLLECTSTATIC" = "xon" ]; then
-	/venv/bin/python manage.py collectstatic --noinput --no-post-process
-fi
-if [ "x$MANAGEPY_COMPRESS" = "xon" ]; then
-	/venv/bin/python manage.py compress --extension=".haml" --force -v0
-fi
+#if [ "x$MANAGEPY_COLLECTSTATIC" = "xon" ]; then
+#	/venv/bin/python manage.py collectstatic --noinput --no-post-process
+#fi
+#if [ "x$MANAGEPY_COMPRESS" = "xon" ]; then
+#	/venv/bin/python manage.py compress --extension=".haml" --force -v0
+#fi
 if [ "x$MANAGEPY_INIT_DB" = "xon" ]; then
 	set +x  # make sure the password isn't echoed to stdout
 	echo "*:*:*:*:$(echo \"$DATABASE_URL\" | cut -d'@' -f1 | cut -d':' -f3)" > $HOME/.pgpass
