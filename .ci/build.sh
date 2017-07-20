36e2a63df905353b683cc14046846ffe6f6acff3
Erik Harding <erik@praekelt.org>
2017-07-20 13:55:00 +0200

    uppercase

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 version=$(curl -s https://api.github.com/repos/praekeltfoundation/rapidpro/tags  | jq -r '.[0].name')
-suffix=$version_suffix
 
-echo "$version.$suffix" > image-params/tag
+echo "$version.$VERSION_SUFFIX" > image-params/tag
