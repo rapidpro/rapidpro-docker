351d80c4f8705fda7afd717c9a31ce980284227b
Erik Harding <erik@praekelt.org>
2017-07-20 13:51:42 +0200

    use named and moved default

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 version=$(curl -s https://api.github.com/repos/praekeltfoundation/rapidpro/tags  | jq -r '.[0].name')
-suffix=${1:-local}
+suffix=$version_suffix
 
 echo "$version.$suffix" > image-params/tag
