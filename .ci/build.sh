12ae76949be5bf6077586f7918c7f645a19cf627
Erik Harding <erik@praekelt.org>
2017-07-20 14:45:17 +0200

    install curl and jq

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+apt-get -qq update
+apt-get -qq -y install curl
+
+curl -o /usr/bin/jq http://stedolan.github.io/jq/download/linux64/jq && chmod +x /usr/bin/jq
+
 version=$(curl -s https://api.github.com/repos/praekeltfoundation/rapidpro/tags  | jq -r '.[0].name')
 
 echo "$version.$VERSION_SUFFIX" > image-params/tag
