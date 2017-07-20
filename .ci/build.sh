61eb69268217872a4c86491fb420091e5a49bbd6
Erik Harding <erik@praekelt.org>
2017-07-20 15:03:50 +0200

    fix install

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-apt-get -qq update
-apt-get -qq -y install curl
+opkg-install curl
 
 curl -o /usr/bin/jq http://stedolan.github.io/jq/download/linux64/jq && chmod +x /usr/bin/jq
 
 version=$(curl -s https://api.github.com/repos/praekeltfoundation/rapidpro/tags  | jq -r '.[0].name')
 
-echo "$version.$VERSION_SUFFIX" > image-params/tag
+VERSION_SUFFIX=${VERSION_SUFFIX+.$VERSION_SUFFIX}
+
+echo "$version$VERSION_SUFFIX" > image-params/tag
