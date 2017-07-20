773fbb036eb1f69192279d5b11e6197f73d09a8e
Erik Harding <erik@praekelt.org>
2017-07-20 12:46:19 +0200

    make suffix configurable

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 version=$(curl -s https://api.github.com/repos/praekeltfoundation/rapidpro/tags  | jq -r '.[0].name')
-postfix=${1:-local}
+suffix=${1:-local}
 
-echo "$version.$postfix" > image-params/tag
+echo "$version.$suffix" > image-params/tag
