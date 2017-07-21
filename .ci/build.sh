874d98eb0db810c29d0811c409df4afef53069e2
Erik Harding <erik@praekelt.org>
2017-07-21 16:11:32 +0200

    remove cat

--- .ci/build.sh
+++ .ci/build.sh
@@ -10,5 +10,3 @@ echo "$version$VERSION_SUFFIX" > image-params/tag
 echo "latest$VERSION_SUFFIX" > image-params/additional_tags
 
 echo '{"RAPIDPRO_VERSION":"'$version'","RAPIDPRO_REPO":"praekeltfoundation/rapidpro"}' > image-params/build-args.json
-
-cat image-params/build-args.json
