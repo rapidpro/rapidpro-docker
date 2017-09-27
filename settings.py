1e7e00610e3b99ddf54e7344660c3bfdc31b01a2
Erik Harding <erik@praekelt.org>
2017-09-27 16:04:47 +0200

    comment for variable name

--- settings.py
+++ settings.py
@@ -73,6 +73,9 @@ if AWS_STORAGE_BUCKET_NAME:
     # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
     # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
     # We also use it in the next setting.
+    # If we call this setting `AWS_S3_CUSTOM_DOMAIN`, that breaks presigned URLs in
+    # django-storages. Use our own setting for the domain instead, which is unknown to
+    # django-storages.
     if CDN_DOMAIN_NAME:
         AWS_S3_DOMAIN = CDN_DOMAIN_NAME
     else:
