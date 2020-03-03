9a00a15f4abb7d717f19892ee98bcd8f8e228c98
Ryan Fischbach <ryan.fischbach@istresearch.com>
2020-03-03 10:25:34 -0500

    remove PM channel from list.

--- settings.py
+++ settings.py
@@ -189,7 +189,6 @@ for brand in BRANDING.values():
     COMPRESS_OFFLINE_CONTEXT.append(context)
 
 CHANNEL_TYPES = [
-    "temba.channels.types.postmaster.PostmasterType",
     "temba.channels.types.bandwidth_international.BandwidthInternationalType",
     "temba.channels.types.bandwidth.BandwidthType",
     "temba.channels.types.arabiacell.ArabiaCellType",
