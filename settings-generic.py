8297765d4009706f0c1b4046041c15f31b96db38
Andrew Carter <andrewkcarter@users.noreply.github.com>
2020-03-13 11:20:29 -0400

    Update settings-generic.py

--- settings-generic.py
+++ settings-generic.py
@@ -193,6 +193,7 @@ for brand in BRANDING.values():
     COMPRESS_OFFLINE_CONTEXT.append(context)
 
 CHANNEL_TYPES = [
+    #"temba.channels.types.postmaster.PostmasterType",
     "temba.channels.types.bandwidth_international.BandwidthInternationalType",
     "temba.channels.types.bandwidth.BandwidthType",
     "temba.channels.types.arabiacell.ArabiaCellType",
