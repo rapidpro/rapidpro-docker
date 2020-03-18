a2396dc0b2b023395f50f0244bd0ca43406a2e74
Marti Martinez <martinezah@gmail.com>
2020-03-18 14:21:32 -0700

    Correct django import error

--- stack/startup.py
+++ stack/startup.py
@@ -3,13 +3,13 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'temba.settings'
 import django
 django.setup()
 from django.contrib.auth.management.commands.createsuperuser import get_user_model
-from django.core.exceptions import DoesNotExist
+from django.core.exceptions import ObjectDoesNotExist
 from temba.orgs.models import Org
 
 try:
     superuser = get_user_model().objects.get(username=os.getenv('ADMIN_EMAIL'))
     print('Super user already exists. SKIPPING.')
-except:
+except ObjectDoesNotExist:
     if os.getenv('ADMIN_NAME') and os.getenv('ADMIN_EMAIL') and os.getenv('ADMIN_PSWD') and os.getenv('ADMIN_ORG'):
         print('Creating super user...')
         superuser = get_user_model()._default_manager.db_manager('default').create_superuser(
